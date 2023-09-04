ada3948951d8baac15c1242a96b7c6023cac9947
TBI89 <tomerben89@gmail.com>
2023-09-04 13:20:13 +0300

    New integration tests (post, put and delete)

--- Backend/test/vacations-controller.test.ts
+++ Backend/test/vacations-controller.test.ts
@@ -1,9 +1,10 @@
 import { expect } from "chai";
+import fs from "fs";
 import { describe, it } from "mocha";
+import * as os from "os";
+import path from "path";
 import supertest from "supertest";
 import app from "../src/app";
-import { createReadStream } from "fs";
-import path from "path";
 
 describe("Testing the vacations-controller", () => {
 
@@ -11,7 +12,7 @@ describe("Testing the vacations-controller", () => {
     it("Should return all vacations", async () => {
 
         // Hard codded data for the authorization headers (Note to provide a valid token):
-        const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJJZCI6MTEsImZpcnN0TmFtZSI6IkFkbWluIiwibGFzdE5hbWUiOiJUaGUgYm9zcyIsImVtYWlsIjoidG9tZXJfYmVuMzg0OTFAZ21haWwuY29tIiwicm9sZUlkIjoxfSwiaWF0IjoxNjkzNzU0NjExLCJleHAiOjE2OTM3NzI2MTF9.FWav7IZGZPnatUCSIAUfkfnkXcPPyJfkC_EIWdv-AvE";
+        const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJJZCI6MTEsImZpcnN0TmFtZSI6IkFkbWluIiwibGFzdE5hbWUiOiJUaGUgYm9zcyIsImVtYWlsIjoidG9tZXJfYmVuMzg0OTFAZ21haWwuY29tIiwicm9sZUlkIjoxfSwiaWF0IjoxNjkzODIxNzE2LCJleHAiOjE2OTM4Mzk3MTZ9.DNgTvrAG6wm7ObVHejIWfjRH5P1bX50EK36WYE9sqI0";
 
         // Send the request & the auth headers to the backend:
         const response = await supertest(app.server)
@@ -28,7 +29,7 @@ describe("Testing the vacations-controller", () => {
     it("Should add a new vacation and return it", async () => {
 
         // Hard codded data for the authorization headers (Note to provide a valid token):
-        const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJJZCI6MTEsImZpcnN0TmFtZSI6IkFkbWluIiwibGFzdE5hbWUiOiJUaGUgYm9zcyIsImVtYWlsIjoidG9tZXJfYmVuMzg0OTFAZ21haWwuY29tIiwicm9sZUlkIjoxfSwiaWF0IjoxNjkzNzU0NjExLCJleHAiOjE2OTM3NzI2MTF9.FWav7IZGZPnatUCSIAUfkfnkXcPPyJfkC_EIWdv-AvE";
+        const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJJZCI6MTEsImZpcnN0TmFtZSI6IkFkbWluIiwibGFzdE5hbWUiOiJUaGUgYm9zcyIsImVtYWlsIjoidG9tZXJfYmVuMzg0OTFAZ21haWwuY29tIiwicm9sZUlkIjoxfSwiaWF0IjoxNjkzODIxNzE2LCJleHAiOjE2OTM4Mzk3MTZ9.DNgTvrAG6wm7ObVHejIWfjRH5P1bX50EK36WYE9sqI0";
 
         // Hard codded data for a new vacation object:
         const vacation = {
@@ -39,11 +40,14 @@ describe("Testing the vacations-controller", () => {
             price: 6500
         };
 
-        // Mock image file path:
-        const imageFilePath = path.resolve(__dirname, "../1-assets/images/mockImage.jpg");
+        // Buffer with dummy image data:
+        const imageBuffer = Buffer.from("image-data.jpg");
 
-        // Create the mock image file:
-        const imageBuffer = createReadStream(imageFilePath);
+        // Path to save the mock image:
+        const mockImagePath = path.resolve(__dirname, "../src/1-assets/images/mockImage.jpg");
+
+        // Create the mock file:
+        fs.writeFileSync(mockImagePath, imageBuffer);
 
         // Send the request to the backend:
         const response = await supertest(app.server)
@@ -54,13 +58,74 @@ describe("Testing the vacations-controller", () => {
             .field("startingDate", vacation.startingDate)
             .field("endingDate", vacation.endingDate)
             .field("price", vacation.price)
-            .attach("image", imageBuffer, "mockImage.jpg");
-            console.log('Response Headers:', response.headers);
+            .attach("image", mockImagePath);
 
         // Expect the object to have a body and id:
         const addedVacation = response.body;
         expect(addedVacation).to.haveOwnProperty("vacationId");
-        expect(addedVacation).to.contain(vacation);
+
+        // Verify that the parsed price from the response matches the expected price in the test data:
+        expect(parseFloat(addedVacation.price)).to.equal(vacation.price);
+    });
+
+    // PUT - update existing vacation:
+    it("Should update an existing vacation", async () => {
+
+        // Hard coded data for authorization headers:
+        const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJJZCI6MTEsImZpcnN0TmFtZSI6IkFkbWluIiwibGFzdE5hbWUiOiJUaGUgYm9zcyIsImVtYWlsIjoidG9tZXJfYmVuMzg0OTFAZ21haWwuY29tIiwicm9sZUlkIjoxfSwiaWF0IjoxNjkzODIxNzE2LCJleHAiOjE2OTM4Mzk3MTZ9.DNgTvrAG6wm7ObVHejIWfjRH5P1bX50EK36WYE9sqI0";
+
+        // Hard coded data for an updated vacation:
+        const updatedVacation = {
+            destination: "London",
+            description: "The city of Harry Potter",
+            startingDate: "2024-01-01",
+            endingDate: "2024-01-10",
+            price: 9500
+        };
+
+        // Buffer for dummy image data:
+        const imageBuffer = Buffer.from("updated-image-data.jpg");
+
+        // Create a temporary file to store the image:
+        const tempImagePath = path.join(os.tmpdir(), "updatedMockImage.jpg");
+        fs.writeFileSync(tempImagePath, imageBuffer);
+
+        // Send request to update vacation:
+        const response = await supertest(app.server)
+            .put("/api/vacations/1")
+            .set("Authorization", token)
+            .field("destination", updatedVacation.destination)
+            .field("description", updatedVacation.description)
+            .field("startingDate", updatedVacation.startingDate)
+            .field("endingDate", updatedVacation.endingDate)
+            .field("price", updatedVacation.price)
+            .attach("image", tempImagePath); // Attach the temporary file.
+
+        // Expect the object to have a body & id:
+        const updatedVacationResponse = response.body;
+        expect(updatedVacationResponse).to.haveOwnProperty("vacationId");
+
+        // Verify that the parsed price from the response matches the updated price from the test data:
+        expect(parseFloat(updatedVacationResponse.price)).to.equal(updatedVacation.price);
+
+        // Clean up: Remove the temporary file
+        fs.unlinkSync(tempImagePath);
+    });
+
+    // DELETE - existing vacation:
+    it("Should delete an existing vacation", async () => {
+
+        // Hard coded data for the authorization headers:
+        const token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJJZCI6MTEsImZpcnN0TmFtZSI6IkFkbWluIiwibGFzdE5hbWUiOiJUaGUgYm9zcyIsImVtYWlsIjoidG9tZXJfYmVuMzg0OTFAZ21haWwuY29tIiwicm9sZUlkIjoxfSwiaWF0IjoxNjkzODIxNzE2LCJleHAiOjE2OTM4Mzk3MTZ9.DNgTvrAG6wm7ObVHejIWfjRH5P1bX50EK36WYE9sqI0";
+
+        // Send DELETE request:
+        const response = await supertest(app.server)
+            .delete("/api/vacations/1")
+            .set("Authorization", token)
+
+        // Expect 204 (No content):
+        expect(response.status).to.equal(204);
+
     });
 
 });
